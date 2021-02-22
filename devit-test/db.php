668ab848824af025d53484dad64b85d58b0ac499
Expelled55 <79444525+Expelled55@users.noreply.github.com>
2021-02-22 10:36:46 +0200

    devit-test

---
+++ devit-test/db.php
@@ -0,0 +1,14 @@
+<?php
+// Подключаем библиотеку RedBeanPHP
+require "libs/rb-mysql.php";
+
+// Подключаемся к БД
+R::setup( 'mysql:host=localhost;dbname=register-bd',
+        'root', 'root' );
+
+// Проверка подключения к БД
+if(!R::testConnection()) die('No DB connection!');
+
+session_start(); // Создаем сессию для авторизации
+
+?>
